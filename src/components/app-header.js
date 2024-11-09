613470e170cef0fcfa84638fcee7e3b2bfc9e8fd
Moldir <sheksizm@MacBook-Air-Moldir.local>
2024-11-09 09:23:56 +0500

    step14

--- src/components/app-header.js
+++ src/components/app-header.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
+import './item-staus-filter.css';
 
-const AppHeader = () => {
-    return <h1>My Todo List</h1>;
+const ItemStatusFilter = () => {
+    return (
+        <div className={"btn-group"}>
+            <button type={"button"}
+                    className={"btn btn-info"}>All list</button>
+            <button type={"button"}
+                    className={"btn btn-outline-secondary"}>Active list</button>
+            <button type={"button"}
+                    className={"btn btn-outline-secondary"}>Done</button>
+        </div>
+    );
 };
-
-export default AppHeader;
+export default ItemStatusFilter;
